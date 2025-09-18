4fa20901e322634e29f16cbe438c02583d55ab5c
Noah A. B. <minerva.bionic@gmail.com>
2025-09-18 13:19:39 +0330

    feat: export react modules from index

--- src/index.ts
+++ src/index.ts
@@ -3,3 +3,13 @@ export * from "./center";
 export * from "./computed";
 export * from "./store";
 export * from "./task-manager";
+
+export * from "./react/state-manager";
+export * from "./react/computed";
+export * from "./react/store";
+export * from "./react/task-manager";
+export * from "./react/form";
+
+export * from "./react/dehydrate";
+export * from "./react/useDisableLog";
+export * from "./react/dev-tool/index";
